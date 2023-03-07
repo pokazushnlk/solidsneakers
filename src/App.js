4e49c97a16055b86d2ffde1f7ef2bb958fa56d1b
Новый пользователь <user@MacBook-Pro-Novyj.local>
2023-03-07 23:09:42 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return( 
+<div className="wrapper-header">
+    <header>
+      <div className="header-left">
+      <img width={40} height={40} src="/img/image.svg"></img>
+        <div className="header-info">
+          <h3>Solid Sneakers</h3>
+          <p>Брендовые кроссовки</p>
+        </div>
+        </div>
+        <ul className="header-right">
+          <li>
+              <img width={18}  height={18} src="/img/cart.svg"></img>
+              <span>1205 руб.</span>
+          </li>
+          <li>
+              <img width={18}  height={18} src="/img/profile.svg"></img>
+          </li>
+        </ul>
+    </header>
+    <div className="content">
+      <h1 className="mb-40">Все кроссовки</h1>
+     
+
+      <div className="sneakers">
+      <div className="card">
+        <img width={133} height={112} src="/img/sneakers/1.jpg"></img>
+        <h5>
+          Мужские Кроссовки Under Armour Curry 8
+        </h5>
+        <div className="card-bottom">
+          <div className="card-content">
+            <span>
+              Цена: 
+            </span>
+            <b>12990 руб.</b>
+          </div>
+          <button className="button">
+            <img width={11} height={11} src="/img/addbutton.svg"></img>
+          </button>
+        </div>
+      </div>
+     
+
+      <div className="card">
+        <img width={133} height={112} src="/img/sneakers/2.jpg"></img>
+        <h5>
+          Мужские Кроссовки Under Armour Curry 8
+        </h5>
+        <div className="card-bottom">
+          <div className="card-content">
+            <span>
+              Цена: 
+            </span>
+            <b>12990 руб.</b>
+          </div>
+          <button className="button">
+            <img width={11} height={11} src="/img/addbutton.svg"></img>
+          </button>
+        </div>
+      </div>
+     
+
+      <div className="card">
+        <img width={133} height={112} src="/img/sneakers/3.jpg"></img>
+        <h5>
+          Мужские Кроссовки Under Armour Curry 8
+        </h5>
+        <div className="card-bottom">
+          <div className="card-content">
+            <span>
+              Цена: 
+            </span>
+            <b>12990 руб.</b>
+          </div>
+          <button className="button">
+            <img width={11} height={11} src="/img/addbutton.svg"></img>
+          </button>
+        </div>
+      </div>
+
+      
+      <div className="card">
+        <img width={133} height={112} src="/img/sneakers/4.jpg"></img>
+        <h5>
+          Мужские Кроссовки Under Armour Curry 8
+        </h5>
+        <div className="card-bottom">
+          <div className="card-content">
+            <span>
+              Цена: 
+            </span>
+            <b>12990 руб.</b>
+          </div>
+          <button className="button">
+            <img width={11} height={11} src="/img/addbutton.svg"></img>
+          </button>
+        </div>
+      </div>
+      
+      
+      </div>
+   
     </div>
+
+</div>
   );
 }
 
